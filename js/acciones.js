1727ea8229d40949fa52ada7ea4f450c07ff6c16
ares21 <aridsaupoo@gmail.com>
2016-05-11 09:39:01 -0500

    3subida
    octava aplicacion

--- js/acciones.js
+++ js/acciones.js
@@ -16,7 +16,7 @@ $(document).ready(function(e) {
 					break;
 					
 					case 2:
-					navigator.notification.vibrate(1);
+					navigator.notification.vibrate(1000);
 					break;
 				}
 			},"Aplicacion8","Beep,Vibrar,Cancelar");
